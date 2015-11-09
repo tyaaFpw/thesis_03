68a8cae73f2f9b1b7e0da41334451421814aa093
Gratia <gratia@icehousecorp.com>
2015-11-09 13:27:08 +0700

    Initial Commit

---
+++ Absent/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Absent
+//
+//  Created by Gratia on 11/9/15.
+//  Copyright © 2015 Gratia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Absent/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Absent
+//
+//  Created by Gratia on 11/9/15.
+//  Copyright © 2015 Gratia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
